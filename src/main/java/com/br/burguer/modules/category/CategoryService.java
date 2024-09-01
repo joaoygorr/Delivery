ad5f5649757d39c801fcaec039ba308265608afc
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-01 15:36:06 -0400

    feat: add service

---
+++ src/main/java/com/br/burguer/modules/category/CategoryService.java
@@ -0,0 +1,7 @@
+package com.br.burguer.modules.category;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
---
+++ src/main/java/com/br/burguer/modules/product/ProductService.java
@@ -0,0 +1,7 @@
+package com.br.burguer.modules.product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+}
