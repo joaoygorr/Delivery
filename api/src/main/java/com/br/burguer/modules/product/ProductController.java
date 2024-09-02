0c0f0bdbf0971292aff5f07074d7566df9b98e3c
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-01 21:44:49 -0400

    feat: find all product

--- api/src/main/java/com/br/burguer/modules/product/ProductController.java
+++ api/src/main/java/com/br/burguer/modules/product/ProductController.java
@@ -1,9 +1,28 @@
 package com.br.burguer.modules.product;
 
+import com.br.burguer.modules.product.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> getAllProducts(Pageable pageable) {
+        Page<ProductDTO> productDTOS = productService.getAllProduct(pageable);
+        return ResponseEntity.ok(productDTOS);
+    }
 }
--- api/src/main/java/com/br/burguer/modules/product/ProductService.java
+++ api/src/main/java/com/br/burguer/modules/product/ProductService.java
@@ -1,7 +1,24 @@
 package com.br.burguer.modules.product;
 
+import com.br.burguer.modules.product.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Page<ProductDTO> getAllProduct(Pageable pageable) {
+        Page<Product> productPage = this.productRepository.findAll(pageable);
+        return productPage.map(ProductDTO::toDto);
+    }
+
 }
