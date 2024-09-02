56bbc0c8894d06e97251542a664b9bb693f5c016
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-01 23:00:41 -0400

    feat: get all category

--- api/src/main/java/com/br/burguer/modules/category/CategoryController.java
+++ api/src/main/java/com/br/burguer/modules/category/CategoryController.java
@@ -1,9 +1,28 @@
 package com.br.burguer.modules.category;
 
+import com.br.burguer.modules.category.dto.CategoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryDTO>> getAll(Pageable pageable) {
+        Page<CategoryDTO> categoryDTOS = this.categoryService.getAllCategories(pageable);
+        return ResponseEntity.ok(categoryDTOS);
+    }
 }
--- api/src/main/java/com/br/burguer/modules/category/CategoryService.java
+++ api/src/main/java/com/br/burguer/modules/category/CategoryService.java
@@ -1,7 +1,23 @@
 package com.br.burguer.modules.category;
 
+import com.br.burguer.modules.category.dto.CategoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Page<CategoryDTO> getAllCategories(Pageable pageable) {
+        Page<Category> categories = this.categoryRepository.findAll(pageable);
+        return categories.map(CategoryDTO::toDto);
+    }
 }
