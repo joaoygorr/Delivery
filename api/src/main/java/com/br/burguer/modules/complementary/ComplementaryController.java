4ee4b0fbae1e3ba90c4d878516ac94f4c58b7574
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-01 22:51:37 -0400

    feat: create getAll

--- api/src/main/java/com/br/burguer/modules/complementary/ComplementaryController.java
+++ api/src/main/java/com/br/burguer/modules/complementary/ComplementaryController.java
@@ -1,9 +1,28 @@
 package com.br.burguer.modules.complementary;
 
+import com.br.burguer.modules.complementary.dto.ComplementaryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/complementary")
+@RequestMapping("/complementary")
 public class ComplementaryController {
+
+    private final ComplementaryService complementaryService;
+
+    @Autowired
+    public ComplementaryController(ComplementaryService complementaryService) {
+        this.complementaryService = complementaryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ComplementaryDTO>> getAll(Pageable pageable) {
+        Page<ComplementaryDTO> complementaryDTOS = this.complementaryService.getAllComplementaries(pageable);
+        return ResponseEntity.ok(complementaryDTOS);
+    }
 }
--- api/src/main/java/com/br/burguer/modules/complementary/ComplementaryService.java
+++ api/src/main/java/com/br/burguer/modules/complementary/ComplementaryService.java
@@ -1,7 +1,23 @@
 package com.br.burguer.modules.complementary;
 
+import com.br.burguer.modules.complementary.dto.ComplementaryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComplementaryService {
+
+    private final ComplementaryRepository complementaryRepository;
+
+    @Autowired
+    public ComplementaryService(ComplementaryRepository complementaryRepository) {
+        this.complementaryRepository = complementaryRepository;
+    }
+
+    public Page<ComplementaryDTO> getAllComplementaries(Pageable pageable) {
+        Page<Complementary> complementaries = this.complementaryRepository.findAll(pageable);
+        return complementaries.map(ComplementaryDTO::toDto);
+    }
 }
