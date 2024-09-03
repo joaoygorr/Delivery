a8df262c62c6db41e461a1063279ed70f9ddb0e1
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-03 19:19:25 -0400

    fix: implements contract

--- api/src/main/java/com/br/burguer/services/product/ProductService.java
+++ api/src/main/java/com/br/burguer/services/product/ProductService.java
@@ -1,32 +1,12 @@
 package com.br.burguer.services.product;
 
-import com.br.burguer.exceptions.Exception404;
-import com.br.burguer.modules.product.Product;
-import com.br.burguer.modules.product.ProductRepository;
 import com.br.burguer.modules.product.dto.ProductDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-@Service
-public class ProductService {
+public interface ProductService {
 
-    private final ProductRepository productRepository;
+    Page<ProductDTO> getAllProducts(Pageable pageable);
 
-    @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    public Page<ProductDTO> getAllProducts(Pageable pageable) {
-        Page<Product> productPage = this.productRepository.findAll(pageable);
-        return productPage.map(ProductDTO::toDto);
-    }
-
-    public ProductDTO getProductById(Long productId) {
-        Product product = this.productRepository.findById(productId)
-                .orElseThrow(() -> new Exception404("Product with code " + productId + " not found!"));
-        return ProductDTO.toDto(product);
-    }
+    ProductDTO getProductById(Long productId);
 }
---
+++ api/src/main/java/com/br/burguer/services/product/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.br.burguer.services.product;
+
+import com.br.burguer.exceptions.Exception404;
+import com.br.burguer.modules.product.Product;
+import com.br.burguer.modules.product.ProductRepository;
+import com.br.burguer.modules.product.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Page<ProductDTO> getAllProducts(Pageable pageable) {
+        Page<Product> productPage = this.productRepository.findAll(pageable);
+        return productPage.map(ProductDTO::toDto);
+    }
+
+    @Override
+    public ProductDTO getProductById(Long productId) {
+        Product product = this.productRepository.findById(productId)
+                .orElseThrow(() -> new Exception404("Product with code " + productId + " not found!"));
+        return ProductDTO.toDto(product);
+    }
+}
