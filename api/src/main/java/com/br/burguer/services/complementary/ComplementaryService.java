8ca512ad022e34ce0735564718608ffbafeecfd9
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-03 19:21:48 -0400

    fix: implements contract

--- api/src/main/java/com/br/burguer/services/complementary/ComplementaryService.java
+++ api/src/main/java/com/br/burguer/services/complementary/ComplementaryService.java
@@ -1,32 +1,12 @@
 package com.br.burguer.services.complementary;
 
-import com.br.burguer.exceptions.Exception404;
-import com.br.burguer.modules.complementary.Complementary;
-import com.br.burguer.modules.complementary.ComplementaryRepository;
 import com.br.burguer.modules.complementary.dto.ComplementaryDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-@Service
-public class ComplementaryService {
+public interface ComplementaryService {
 
-    private final ComplementaryRepository complementaryRepository;
+    Page<ComplementaryDTO> getAllComplementaries(Pageable pageable);
 
-    @Autowired
-    public ComplementaryService(ComplementaryRepository complementaryRepository) {
-        this.complementaryRepository = complementaryRepository;
-    }
-
-    public Page<ComplementaryDTO> getAllComplementaries(Pageable pageable) {
-        Page<Complementary> complementaries = this.complementaryRepository.findAll(pageable);
-        return complementaries.map(ComplementaryDTO::toDto);
-    }
-
-    public ComplementaryDTO getComplementaryById(Long complementaryId) {
-        Complementary complementary = this.complementaryRepository.findById(complementaryId)
-                .orElseThrow(() -> new Exception404("Complementary with code " + complementaryId + " not found!"));
-        return ComplementaryDTO.toDto(complementary);
-    }
+    ComplementaryDTO getComplementaryById(Long complementaryId);
 }
---
+++ api/src/main/java/com/br/burguer/services/complementary/ComplementaryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.br.burguer.services.complementary;
+
+import com.br.burguer.exceptions.Exception404;
+import com.br.burguer.modules.complementary.Complementary;
+import com.br.burguer.modules.complementary.ComplementaryRepository;
+import com.br.burguer.modules.complementary.dto.ComplementaryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComplementaryServiceImpl implements ComplementaryService {
+
+    private final ComplementaryRepository complementaryRepository;
+
+    @Autowired
+    public ComplementaryServiceImpl(ComplementaryRepository complementaryRepository) {
+        this.complementaryRepository = complementaryRepository;
+    }
+
+    @Override
+    public Page<ComplementaryDTO> getAllComplementaries(Pageable pageable) {
+        Page<Complementary> complementaries = this.complementaryRepository.findAll(pageable);
+        return complementaries.map(ComplementaryDTO::toDto);
+    }
+
+    @Override
+    public ComplementaryDTO getComplementaryById(Long complementaryId) {
+        Complementary complementary = this.complementaryRepository.findById(complementaryId)
+                .orElseThrow(() -> new Exception404("Complementary with code " + complementaryId + " not found!"));
+        return ComplementaryDTO.toDto(complementary);
+    }
+}
