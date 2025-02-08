20f48f08c1461d58bf91baf6f22f331eafacea23
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-08 16:39:19 -0400

    feat: create page login

--- app/src/app/(auth)/signIn/page.tsx
+++ app/src/app/(auth)/signIn/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Link as MuiLink,
+} from "@mui/material";
+import Link from "next/link";
+import "./signIn.scss";
+
 export default function Page() {
-  return <div>Singn In</div>;
+  return (
+    <div>
+      <Typography component="p" className="text-auth">
+        Digite seus dados para entrar no painel administrativo e gerenciar
+        produtos/pedidos.
+      </Typography>
+
+      <Box component="form" className="box-form">
+        <TextField
+          label="Digite seu e-mail"
+          name="email"
+          required
+          fullWidth
+          autoFocus
+        />
+
+        <TextField
+          label="Digite sua senha"
+          name="password"
+          type="password"
+          required
+          fullWidth
+        />
+        <Button type="submit" variant="contained" fullWidth>
+          entrar
+        </Button>
+
+        <Box className="box-forgot">
+          <MuiLink href="/login/forgot" variant="body2" component={Link}>
+            Esqueceu sua senha?
+          </MuiLink>
+        </Box>
+      </Box>
+    </div>
+  );
 }
