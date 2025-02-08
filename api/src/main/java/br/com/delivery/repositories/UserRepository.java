8797b62047405e6e5825aca54739dac6aab77714
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-08 18:56:08 -0400

    feat: create repository

---
+++ api/src/main/java/br/com/delivery/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.delivery.repositories;
+
+import br.com.delivery.modules.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
