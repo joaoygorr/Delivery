aa96496d1844844270b031dc786080ee1b034147
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-08 17:27:41 -0400

    feat: create schema auth

---
+++ app/src/shared/schemas/authForm.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const createAuthFormSchema = z.object({
+    email: z.string()
+        .nonempty("O e-mail é obrigatório")
+        .email("Formato de e-mail inválido"),
+    password: z.string()
+        .min(5, "A senha precisa de no mínimo 5 caracteres")
+        .optional()
+});
+
+export default createAuthFormSchema;
