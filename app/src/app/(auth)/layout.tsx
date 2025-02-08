9746875d4672674bc73bad85bbc184a866e3f5b7
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-08 16:39:29 -0400

    feat: create layout

--- app/src/app/(auth)/layout.tsx
+++ app/src/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { ReactNode } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Box, Container, Typography } from "@mui/material";
+import "./layout.scss";
 
 type Props = {
   children: ReactNode;
@@ -11,27 +13,16 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <html lang="pt-br">
-      <body>
-        <Container component="main" maxWidth="xs">
-          <Box
-            sx={{
-              mt: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h3" variant="h3">
-              Delivery
-            </Typography>
-            <Typography component="h3" variant="h3">
-              Painel do estabelecimento
-            </Typography>
-            {children}
-          </Box>
-        </Container>
-      </body>
-    </html>
+    <Container component="main" maxWidth="xs">
+      <Box className="box-layout">
+        <Typography component="h3" variant="h3">
+          Delivery
+        </Typography>
+        <Typography component="h5" variant="h5">
+          Painel do estabelecimento
+        </Typography>
+        {children}
+      </Box>
+    </Container>
   );
 }
