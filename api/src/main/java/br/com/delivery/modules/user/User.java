5a7aa618114372060e84897ef404b665d10eaa61
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-09 13:35:13 -0400

    feat: config enum

--- api/src/main/java/br/com/delivery/modules/user/User.java
+++ api/src/main/java/br/com/delivery/modules/user/User.java
@@ -30,6 +30,7 @@ public class User implements UserDetails {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     public User(String username, String email, String password) {
