8a0005dd488bc3d4a55b63389c4da52cbf58643b
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-09 00:35:22 -0400

    feat: auth service

---
+++ api/src/main/java/br/com/delivery/services/auth/AuthorizationService.java
@@ -0,0 +1,32 @@
+package br.com.delivery.services.auth;
+
+import br.com.delivery.modules.user.User;
+import br.com.delivery.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(("Usuário não encontrado")));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), new ArrayList<>());
+    }
+}
