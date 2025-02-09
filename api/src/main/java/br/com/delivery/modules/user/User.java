6232401c1c151f0970f5297fbf184bc579228a2b
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-09 01:37:09 -0400

    fix: user setter

--- api/src/main/java/br/com/delivery/modules/user/User.java
+++ api/src/main/java/br/com/delivery/modules/user/User.java
@@ -2,10 +2,7 @@ package br.com.delivery.modules.user;
 
 import br.com.delivery.modules.user.enuns.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +13,7 @@ import java.util.List;
 @Table(name = "users")
 @Entity(name = "users")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
