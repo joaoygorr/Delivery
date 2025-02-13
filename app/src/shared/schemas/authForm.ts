5a4dcd982324cefebf465e67a192d4a242b5a59f
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-13 18:29:27 -0400

    feat: campoUsername

--- app/src/shared/schemas/authForm.ts
+++ app/src/shared/schemas/authForm.ts
@@ -10,6 +10,10 @@ const createAuthFormSchema = z.object({
     confirmPassword: z.string()
         .min(5, "A senha precisa de no mínimo 5 caracteres")
         .optional(),
+    userName: z.string()
+        .trim()
+        .min(1, { message: "O nome não pode estar vazio" })
+        .regex(/^[A-za-zÀ-ÖØ-öø-ÿ\s]+$/, { message: "O nome deve conter apenas letras e espaços" })
 }).refine((data) => !data.confirmPassword || data.password === data.confirmPassword, {
     message: "A senha e a confirmação de senha precisam ser iguais",
     path: ["confirmPassword"]
