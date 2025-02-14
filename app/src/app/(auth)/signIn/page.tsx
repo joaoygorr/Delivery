49840d98419c09aabdd3f14f36cc824ae7fff373
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-13 22:30:17 -0400

    chore: remove detalhes

--- app/src/app/(auth)/signIn/page.tsx
+++ app/src/app/(auth)/signIn/page.tsx
@@ -40,72 +40,70 @@ export default function Page() {
       console.log(e);
     }
   }
-  console.log(errors);
 
   return (
-    <div className="teste">
-      <Card variant="outlined" className="box-card">
-        <Typography component="h1" variant="h4" className="text-auth">
-          Login
-        </Typography>
-        <Box
-          component="form"
-          className="box-form"
-          noValidate
-          onSubmit={handleSubmit(submitLogin)}
+    <Card variant="outlined" className="box-card">
+      <Typography component="h1" variant="h4" className="text-auth">
+        Login
+      </Typography>
+      <Box
+        component="form"
+        className="box-form"
+        noValidate
+        onSubmit={handleSubmit(submitLogin)}
+      >
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextField
+            placeholder="your@email.com"
+            autoComplete="email"
+            fullWidth
+            helperText={errors.email?.message}
+            autoFocus
+            color={errors?.email ? "error" : "primary"}
+            variant="outlined"
+            {...register("email")}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="password">Senha</FormLabel>
+          <TextField
+            type="password"
+            placeholder="••••••"
+            helperText={errors.password?.message}
+            fullWidth
+            variant="outlined"
+            color={errors?.password ? "error" : "primary"}
+            {...register("password")}
+          />
+        </FormControl>
+
+        <ForgotPassword open={open} handleClose={setOpen} />
+
+        <Button type="submit" variant="contained" fullWidth>
+          entrar
+        </Button>
+
+        <MuiLink
+          component="button"
+          type="button"
+          variant="body2"
+          className="link-forgot"
+          onClick={() => setOpen(true)}
         >
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <TextField
-              placeholder="your@email.com"
-              fullWidth
-              helperText={errors.email?.message}
-              autoFocus
-              color={errors?.email ? "error" : "primary"}
-              variant="outlined"
-              {...register("email")}
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <TextField
-              type="password"
-              placeholder="••••••"
-              helperText={errors.password?.message}
-              fullWidth
-              variant="outlined"
-              color={errors?.password ? "error" : "primary"}
-              {...register("password")}
-            />
-          </FormControl>
-
-          <ForgotPassword open={open} handleClose={setOpen} />
-
-          <Button type="submit" variant="contained" fullWidth>
-            entrar
-          </Button>
-
-          <MuiLink
-            component="button"
-            type="button"
-            variant="body2"
-            className="link-forgot"
-            onClick={() => setOpen(true)}
-          >
-            Esqueceu sua senha?
+          Esqueceu sua senha?
+        </MuiLink>
+      </Box>
+      <Divider>ou</Divider>
+      <Box className="box-auth-forgot">
+        <Typography>
+          Não tem uma conta?{" "}
+          <MuiLink href="/signup" variant="body2" component={Link}>
+            Cadastrar-se
           </MuiLink>
-        </Box>
-        <Divider>ou</Divider>
-        <Box className="box-auth-forgot">
-          <Typography>
-            Não tem uma conta?{" "}
-            <MuiLink href="/signup" variant="body2" component={Link}>
-              Cadastrar-se
-            </MuiLink>
-          </Typography>
-        </Box>
-      </Card>
-    </div>
+        </Typography>
+      </Box>
+    </Card>
   );
 }
