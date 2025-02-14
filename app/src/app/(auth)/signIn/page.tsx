6b0e7cae8adf4b22c59fd8b538013df9cd7f5ade
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-13 22:12:45 -0400

    feat: estilizacao page signIn

--- app/src/app/(auth)/signIn/page.tsx
+++ app/src/app/(auth)/signIn/page.tsx
@@ -5,14 +5,20 @@ import {
   TextField,
   Typography,
   Link as MuiLink,
+  FormControl,
+  FormLabel,
+  Card,
+  Divider,
 } from "@mui/material";
-import Link from "next/link";
 import "./signIn.scss";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import createAuthFormSchema from "@/shared/schemas/authForm";
 import { authFormData } from "@/shared/types/types";
 import { authApi } from "@/shared/api/api";
+import Link from "next/link";
+import ForgotPassword from "@/shared/components/forgotPassword";
+import { useState } from "react";
 
 export default function Page() {
   const {
@@ -23,6 +29,8 @@ export default function Page() {
     resolver: zodResolver(createAuthFormSchema),
   });
 
+  const [open, setOpen] = useState(false);
+
   function submitLogin(data: authFormData) {
     console.log("hi", data);
 
@@ -32,50 +40,72 @@ export default function Page() {
       console.log(e);
     }
   }
+  console.log(errors);
 
   return (
-    <div>
-      <Typography component="p" className="text-auth">
-        Digite seus dados para entrar no painel administrativo e gerenciar
-        produtos/pedidos.
-      </Typography>
+    <div className="teste">
+      <Card variant="outlined" className="box-card">
+        <Typography component="h1" variant="h4" className="text-auth">
+          Login
+        </Typography>
+        <Box
+          component="form"
+          className="box-form"
+          noValidate
+          onSubmit={handleSubmit(submitLogin)}
+        >
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <TextField
+              placeholder="your@email.com"
+              fullWidth
+              helperText={errors.email?.message}
+              autoFocus
+              color={errors?.email ? "error" : "primary"}
+              variant="outlined"
+              {...register("email")}
+            />
+          </FormControl>
 
-      <Box
-        component="form"
-        className="box-form"
-        onSubmit={handleSubmit(submitLogin)}
-      >
-        <TextField
-          label="Digite seu e-mail"
-          fullWidth
-          autoFocus
-          {...register("email")}
-        />
-        {errors.email && <span>{errors.email.message}</span>}
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <TextField
+              type="password"
+              placeholder="••••••"
+              helperText={errors.password?.message}
+              fullWidth
+              variant="outlined"
+              color={errors?.password ? "error" : "primary"}
+              {...register("password")}
+            />
+          </FormControl>
 
-        <TextField
-          label="Digite sua senha"
-          type="password"
-          fullWidth
-          {...register("password")}
-        />
-        {errors.password && <span>{errors.password.message}</span>}
+          <ForgotPassword open={open} handleClose={setOpen} />
 
-        <Button type="submit" variant="contained" fullWidth>
-          entrar
-        </Button>
+          <Button type="submit" variant="contained" fullWidth>
+            entrar
+          </Button>
 
-        <Box className="box-auth-forgot">
-          <MuiLink href="/signin/forgot" variant="body2" component={Link}>
+          <MuiLink
+            component="button"
+            type="button"
+            variant="body2"
+            className="link-forgot"
+            onClick={() => setOpen(true)}
+          >
             Esqueceu sua senha?
           </MuiLink>
         </Box>
+        <Divider>ou</Divider>
         <Box className="box-auth-forgot">
-          <MuiLink href="/signup" variant="body2" component={Link}>
-            realizar cadastro
-          </MuiLink>
+          <Typography>
+            Não tem uma conta?{" "}
+            <MuiLink href="/signup" variant="body2" component={Link}>
+              Cadastrar-se
+            </MuiLink>
+          </Typography>
         </Box>
-      </Box>
+      </Card>
     </div>
   );
 }
