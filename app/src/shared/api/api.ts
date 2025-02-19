d1530ef3c39f9983ee48f82e8e472615f76b69b0
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-18 21:12:15 -0400

    chore: refatora api

--- app/src/shared/api/api.ts
+++ app/src/shared/api/api.ts
@@ -1,51 +1,43 @@
 import axios, { AxiosInstance } from "axios";
 import { authFormData } from "../types/types";
 
-const createApiInstance = (url: string): AxiosInstance => {
-    const api = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_URL + url,
-        headers: { Accept: "application/json" }
-    });
-
-    api.interceptors.request.use((config) => {
-        const token = getToken();
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    });
-
-    return api;
-}
-
-const getToken = (): string | null => {
-    if (typeof window === "undefined") return null; // Evita erro no SSR
-    return localStorage.getItem("token");
-};
-
-const verifyToken = (token?: string): void => {
-    if (!token) return;
-    if (typeof window !== "undefined") {
-        localStorage.setItem("token", token);
-    }
-};
-
 export class Api {
     private api: AxiosInstance;
 
     constructor(url: string) {
-        this.api = createApiInstance(url);
+        this.api = axios.create({
+            baseURL: process.env.NEXT_PUBLIC_URL + url,
+            headers: { Accept: "application/json" }
+        });
+
+        this.api.interceptors.request.use((config) => {
+            const token = this.getToken();
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        });
+    }
+
+    getToken(): string | null {
+        if (typeof window === "undefined") return null;
+        return localStorage.getItem("token");
+    }
+
+    private verifyToken(token?: string): void {
+        if (!token || typeof window === "undefined") return;
+        localStorage.setItem("token", token);
     }
 
     async login(user: authFormData): Promise<{ userName: string, token: string }> {
         const res = await this.api.post("/login", user);
-        verifyToken(res?.data?.token);
+        this.verifyToken(res?.data?.token);
         return res?.data
     }
 
     async register(newUser: authFormData): Promise<{ userName: string, token: string }> {
         const res = await this.api.post("/register", newUser);
-        verifyToken(res?.data?.token);
+        this.verifyToken(res?.data?.token);
         return res.data;
     }
 }
