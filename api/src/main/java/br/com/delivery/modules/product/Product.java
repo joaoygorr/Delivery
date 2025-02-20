b9e5d371e3cbcfb7c9cbb91dc3541f14efd06bec
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-19 23:34:05 -0400

    feat: create product entity

---
+++ api/src/main/java/br/com/delivery/modules/product/Product.java
@@ -0,0 +1,37 @@
+package br.com.delivery.modules.product;
+
+import br.com.delivery.modules.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Table(name = "Products")
+@Entity(name = "Products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idProduct")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String img;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
