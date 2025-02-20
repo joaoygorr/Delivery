d4afb78fcf1ae35607fe8ae4983a8a5812478ed7
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-19 23:22:24 -0400

    feat: create order entity

---
+++ api/src/main/java/br/com/delivery/modules/order/Order.java
@@ -0,0 +1,51 @@
+package br.com.delivery.modules.order;
+
+import br.com.delivery.modules.order.enuns.OrderStatus;
+import br.com.delivery.modules.order.enuns.PaymentType;
+import br.com.delivery.modules.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Table(name = "orders")
+@Entity(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idOrder")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOrder;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "establishment_id", nullable = false)
+    private Establishment establishment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentType paymentMethod;
+
+    private BigDecimal changeAmount;
+
+    @Column(nullable = false)
+    private BigDecimal deliveryPrice;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+}
