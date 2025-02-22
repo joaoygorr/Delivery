0f5ba5d790c7a2b585ef3948293e560eb13d0df7
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 19:00:11 -0400

    fix: troca validacao

--- api/src/main/java/br/com/delivery/records/product/ProductRecord.java
+++ api/src/main/java/br/com/delivery/records/product/ProductRecord.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public record ProductRecord(Long idProduct,
                             @NotBlank(message = "Nome não pode estar vazio") String name,
                             @NotNull(message = "Imagem não pode estar vazia") byte[] img,
-                            @NotBlank(message = "Preço não pode estar vazia") BigDecimal price,
+                            @NotNull(message = "Preço não pode estar vazia") BigDecimal price,
                             @NotBlank(message = "Descrição não pode estar vazia") String description,
                             @NotNull(message = "Categoria não pode estar vazia") Long categoryId) {
 }
