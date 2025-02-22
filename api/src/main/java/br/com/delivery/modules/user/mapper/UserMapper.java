6f3d26fe7c73b9efd11ac1452004ff9febc99e7f
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 19:00:02 -0400

    fix: asjute mapper

--- api/src/main/java/br/com/delivery/modules/user/mapper/UserMapper.java
+++ api/src/main/java/br/com/delivery/modules/user/mapper/UserMapper.java
@@ -1,13 +1,12 @@
 package br.com.delivery.modules.user.mapper;
 
-import br.com.delivery.mapper.DtoMapper;
 import br.com.delivery.modules.user.User;
 import br.com.delivery.records.user.UserRecord;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.ERROR)
-public abstract class UserMapper implements DtoMapper<User, UserRecord> {
+public abstract class UserMapper {
 
     public abstract User toEntityAuth(UserRecord dto);
 }
