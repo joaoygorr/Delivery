c025590f06278031e76f3f0b5ceed533d003b9d4
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 15:21:30 -0400

    feat: create page product

--- app/src/app/(inside)/products/page.tsx
+++ app/src/app/(inside)/products/page.tsx
@@ -1,3 +1,57 @@
+"use client";
+import ProductEditDialog from "@/shared/components/admin/productEditDialog/productEditDialog";
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import "./product.scss";
+
 export default function Page() {
-  return <div>Produtos</div>;
+  const [openDialog, setOpenDialog] = useState(false);
+
+  return (
+    <Box className="box-product">
+      <Box className="box-header-product">
+        <Typography component="h5" variant="h5">
+          Produtos
+        </Typography>
+        <Button onClick={() => setOpenDialog(!openDialog)}>Novo Produto</Button>
+      </Box>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell
+              sx={{ width: 50, display: { xs: "none", md: "table-cell" } }}
+            >
+              ID
+            </TableCell>
+            <TableCell>Image</TableCell>
+            <TableCell>Nome</TableCell>
+            <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+              Preço
+            </TableCell>
+            <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+              Categoria
+            </TableCell>
+            <TableCell sx={{ xs: 50, md: 130 }}>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody></TableBody>
+      </Table>
+
+      <ProductEditDialog
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+      />
+    </Box>
+  );
 }
