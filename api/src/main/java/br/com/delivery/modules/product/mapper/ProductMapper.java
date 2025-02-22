2c104756303bea584f2d449b407ca43e5672a054
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 17:38:25 -0400

    feat: mapper product

--- api/src/main/java/br/com/delivery/modules/product/mapper/ProductMapper.java
+++ api/src/main/java/br/com/delivery/modules/product/mapper/ProductMapper.java
@@ -1,24 +1,29 @@
 package br.com.delivery.modules.product.mapper;
 
+import br.com.delivery.mapper.DtoMapper;
 import br.com.delivery.modules.category.Category;
 import br.com.delivery.modules.product.Product;
 import br.com.delivery.records.product.ProductRecord;
 import br.com.delivery.repositories.CategoryRepository;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.EntityNotFoundException;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.ERROR)
-@RequiredArgsConstructor
-public abstract class ProductMapper {
+public abstract class ProductMapper implements DtoMapper<Product, ProductRecord> {
 
-    private final CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    @Override
     @Mapping(target = "category", source = "categoryId")
-    public abstract Product toEntity(ProductRecord productRecord);
+    public abstract Product toEntity(ProductRecord productRecord, @MappingTarget Product entity);
 
     Category mapCategory(Long id) {
-        return categoryRepository.findById(id);
+        return this.categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Categoria não encontrado"));
     }
 }
