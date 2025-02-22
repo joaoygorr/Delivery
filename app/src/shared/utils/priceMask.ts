445d4c3a8a3a35028c1bb25916c72531595cf88d
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 15:19:45 -0400

    feat: mask price

---
+++ app/src/shared/utils/priceMask.ts
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+
+const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const onlyNumbers = e.target.value.replace(/\D/g, "");
+
+    if (!onlyNumbers) e.target.value = "R$ ";;
+
+    const numericValue = ((parseInt(onlyNumbers, 10)) / 100).toFixed(2);
+
+    e.target.value = `R$ ${numericValue.replace(".", ",")}`;
+}
+
+export default handlePriceChange;
