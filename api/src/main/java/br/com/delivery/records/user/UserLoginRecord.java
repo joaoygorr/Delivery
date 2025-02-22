40a7e7058e7f2bfabbd4c13f97f77a46b40fbb25
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-22 16:02:14 -0400

    fix: create mapper

--- api/src/main/java/br/com/delivery/records/user/UserLoginRecord.java
+++ api/src/main/java/br/com/delivery/records/user/UserLoginRecord.java
@@ -1,7 +1,12 @@
 package br.com.delivery.records.user;
 
+import br.com.delivery.modules.user.User;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginRecord(@NotBlank(message = "E-mail não pode estar em branco") String email,
                               @NotBlank(message = "Senha não pode estar em branco") String password){
+
+    public static User toEntity(UserLoginRecord loginRecord) {
+        return new User(loginRecord.email(), loginRecord.password());
+    }
 }
--- api/src/main/java/br/com/delivery/records/user/UserRegisterRecord.java
+++ api/src/main/java/br/com/delivery/records/user/UserRegisterRecord.java
@@ -1,8 +1,13 @@
 package br.com.delivery.records.user;
 
+import br.com.delivery.modules.user.User;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRegisterRecord(@NotBlank(message = "Nome não pode estar em branco") String userName,
                                  @NotBlank(message = "E-mail não pode estar em branco") String email,
                                  @NotBlank(message = "Senha não pode estar em branco") String password) {
+
+    public static User toEntity(UserRegisterRecord registerRecord) {
+        return new User(registerRecord.userName(), registerRecord.email(), registerRecord.password());
+    }
 }
