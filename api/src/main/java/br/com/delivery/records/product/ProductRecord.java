dd928aa2b8874b255f81e9d665d17e341e8c863b
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-27 10:43:00 -0400

    chore: troca de tipo

--- api/src/main/java/br/com/delivery/records/product/ProductRecord.java
+++ api/src/main/java/br/com/delivery/records/product/ProductRecord.java
@@ -2,12 +2,13 @@ package br.com.delivery.records.product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
 public record ProductRecord(Long idProduct,
                             @NotBlank(message = "Nome não pode estar vazio") String name,
-                            @NotNull(message = "Imagem não pode estar vazia") byte[] img,
+                            @NotNull(message = "Imagem não pode estar vazia") MultipartFile img,
                             @NotNull(message = "Preço não pode estar vazia") BigDecimal price,
                             @NotBlank(message = "Descrição não pode estar vazia") String description,
                             @NotNull(message = "Categoria não pode estar vazia") Long categoryId) {
