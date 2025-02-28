6f5f296cd1f73dd23696dbf187faeff65e23b6eb
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-27 23:57:43 -0400

    fix: ajuste mascara preco

--- app/src/shared/utils/priceMask.ts
+++ app/src/shared/utils/priceMask.ts
@@ -7,7 +7,7 @@ const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
 
     const numericValue = ((parseInt(onlyNumbers, 10)) / 100).toFixed(2);
 
-    e.target.value = `R$ ${numericValue.replace(".", ",")}`;
+    e.target.value = `R$ ${numericValue.replace(".", ",").replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
 }
 
 export default handlePriceChange;
