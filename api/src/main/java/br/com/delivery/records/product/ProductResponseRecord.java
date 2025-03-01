51ce1705d8ef428e4293a14623917c994d6435d5
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-01 14:46:38 -0400

    refactor: remove validacao response

--- api/src/main/java/br/com/delivery/records/product/ProductResponseRecord.java
+++ api/src/main/java/br/com/delivery/records/product/ProductResponseRecord.java
@@ -1,14 +1,11 @@
 package br.com.delivery.records.product;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 import java.math.BigDecimal;
 
 public record ProductResponseRecord(Long idProduct,
-                                    @NotBlank(message = "Nome não pode estar vazio") String name,
-                                    @NotNull(message = "Imagem não pode estar vazia") byte[] img,
-                                    @NotNull(message = "Preço não pode estar vazia") BigDecimal price,
-                                    @NotBlank(message = "Descrição não pode estar vazia") String description,
-                                    @NotNull(message = "Categoria não pode estar vazia") Long categoryId) {
+                                    String name,
+                                    byte[] img,
+                                    BigDecimal price,
+                                    String description,
+                                    Long categoryId) {
 }
