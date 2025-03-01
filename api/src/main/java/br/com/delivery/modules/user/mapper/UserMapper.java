7a243d0c6a7e79be465164927b8fa3cd735652a2
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-01 18:22:26 -0400

    feat: mapper login

--- api/src/main/java/br/com/delivery/modules/user/mapper/UserMapper.java
+++ api/src/main/java/br/com/delivery/modules/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package br.com.delivery.modules.user.mapper;
 
 import br.com.delivery.modules.user.User;
+import br.com.delivery.records.user.LoginRecord;
 import br.com.delivery.records.user.UserRecord;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -8,5 +9,7 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.ERROR)
 public abstract class UserMapper {
 
-    public abstract User toEntityAuth(UserRecord dto);
+    public abstract User toEntityRegister(UserRecord dto);
+
+    public abstract User toEntityLogin(LoginRecord loginRecord);
 }
