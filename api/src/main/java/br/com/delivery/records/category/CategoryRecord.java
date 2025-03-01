6467a9a9ac4b24cd53361520607b36b5eaed0d69
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-01 14:47:25 -0400

    refactor: id name

--- api/src/main/java/br/com/delivery/records/category/CategoryRecord.java
+++ api/src/main/java/br/com/delivery/records/category/CategoryRecord.java
@@ -2,6 +2,6 @@ package br.com.delivery.records.category;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CategoryRecord(Long id,
+public record CategoryRecord(Long idCategory,
                              @NotBlank(message = "Nome não pode estar vazio") String name) {
 }
