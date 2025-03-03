569c6df446410c3d22a80fbdd0aa1b260fdf51fa
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-03 16:30:39 -0400

    chore: renomeia caminho

--- app/src/app/(private)/admin/products/page.tsx
+++ app/src/app/(private)/admin/products/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ProductEditDialog from "@/shared/components/admin/productEditDialog/productEditDialog";
+import ProductEditDialog from "@/shared/components/admin/productDialog/productDialog";
 import {
   Box,
   Button,
