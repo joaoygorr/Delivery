3d6c01d5cb1d23502fa66c09ab0fe29e0b6a5e3a
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-03 16:30:56 -0400

    feat: cria categoria

--- app/src/app/(private)/admin/categories/page.tsx
+++ app/src/app/(private)/admin/categories/page.tsx
@@ -1,3 +1,43 @@
+"use client";
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import "./category.scss";
+import { useState } from "react";
+
 export default function Page() {
-  return <div>Categorias</div>;
+  const [openDialog, setOpenDialog] = useState(false);
+
+  return (
+    <Box className="box-category">
+      <Box className="box-header-category">
+        <Typography component="h5" variant="h5">
+          Categorias
+        </Typography>
+        <Button onClick={() => setOpenDialog(!openDialog)}>
+          Nova Categoria
+        </Button>
+      </Box>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Nome</TableCell>
+
+            <TableCell sx={{ width: 100 }}>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody></TableBody>
+      </Table>
+    </Box>
+  );
 }
