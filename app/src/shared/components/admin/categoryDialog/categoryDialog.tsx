84d7a8c4d3c5a7a25929284ef0a397540c195ca7
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-03 20:54:36 -0400

    feat: dialog category

--- app/src/shared/components/admin/categoryDialog/categoryDialog.tsx
+++ app/src/shared/components/admin/categoryDialog/categoryDialog.tsx
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import "./categoryDialog.scss";
 
 type Props = {
   open: boolean;
@@ -47,7 +48,6 @@ export default function CategoryDialog({
       <DialogContent>
         <Box
           component="form"
-          encType="multipart/form-data"
           onSubmit={handleSubmit(onSave)}
           className="box-form-category"
         >
