ee3535f6c8b9fedd1a9b10699bd0f93f934818c4
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-03 20:54:12 -0400

    feat: transforma primeira caracter em UpperCase

--- app/src/shared/schemas/cateogryForm.ts
+++ app/src/shared/schemas/cateogryForm.ts
@@ -2,7 +2,14 @@ import { z } from "zod";
 
 const createCategoryFormSchema = z.object({
     idCategory: z.number().optional(),
-    name: z.string().trim().nonempty("O nome da categoria é obrigatória")
+    name: z.string()
+        .trim()
+        .nonempty("O nome da categoria é obrigatória")
+        .transform(name => {
+            return name.trim().split(" ").map(word => {
+                return word[0].toLocaleUpperCase().concat(word.substring(1));
+            }).join(" ");
+        })
 })
 
 export default createCategoryFormSchema;
--- app/src/shared/schemas/productForm.ts
+++ app/src/shared/schemas/productForm.ts
@@ -4,7 +4,12 @@ const createProductFormSchema = z.object({
     idProduct: z.number().optional(),
     name: z.string()
         .trim()
-        .nonempty("O nome do produto é obrigatório"),
+        .nonempty("O nome do produto é obrigatório")
+        .transform(name => {
+            return name.trim().split(" ").map(word => {
+                return word[0].toLocaleUpperCase().concat(word.substring(1));
+            }).join(" ");
+        }),
     img: z.union([
         z.instanceof(FileList)
             .transform(list => list.item(0)),
@@ -14,10 +19,11 @@ const createProductFormSchema = z.object({
         .trim()
         .regex(/^R\$\s?\d{1,3}(\.\d{3})*(,\d{2})$/, "Preço inválido"),
     description: z.string()
-        .trim(),
-    categoryId: z.string()
         .trim()
-        .nonempty("A categoria é obrigatória")
+        .transform(desc => {
+            return desc.trim().charAt(0).toLocaleUpperCase() + desc.slice(1);
+        }),
+    categoryId: z.number().min(1, "A categoria é obrigatória")
 })
 
 export default createProductFormSchema; 
