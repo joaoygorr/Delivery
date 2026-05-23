90c7cf44b948f5e88a45b2dc527c03d6f52b4e84
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2026-05-23 15:31:12 -0400

    feat: adiciona config jpa audit e ajuste config gerais

---
+++ api/src/main/java/br/com/delivery/configuration/jpa/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package br.com.delivery.configuration.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
